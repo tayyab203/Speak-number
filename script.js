aa8a7fdd27cf6da532024d3fad58cb983ab5a82f
tayyab203 <61288485+tayyab203@users.noreply.github.com>
2020-05-04 10:57:04 +0500

    commit 3

---
+++ script.js
@@ -0,0 +1,29 @@
+const msgEl = document.getElementById('msg');
+
+const randomNum = getRandomNumber();
+
+console.log('Number:', randomNum);
+
+window.SpeechRecognition =
+  window.SpeechRecognition || window.webkitSpeechRecognition;
+
+let recognition = new window.SpeechRecognition();
+
+// Start recognition and game
+recognition.start();
+
+// Capture user speak
+function onSpeak(e) {
+  const msg = e.results[0][0].transcript;
+
+  writeMessage(msg);
+  checkNumber(msg);
+}
+
+// Write what user speaks
+function writeMessage(msg) {
+  msgEl.innerHTML = `
+    <div>You said: </div>
+    <span class="box">${msg}</span>
+  `;
+}
